8f8e529f69043d6e1a14383d6f796dc9cc7bda7c
leormston <l.e.ormston@gmail.com>
2024-02-06 12:51:34 +0000

    changing the react file a bit

--- aws-stepfunction-sqs-poc/src/App.js
+++ aws-stepfunction-sqs-poc/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import logo from './lg_logo.png';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="box1">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        </div>
+        <p className="midWidth">
+          Proof of concept for AWS Step Functions and SQS and a place to refer to when looking for code snippets
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="box2">
+          <Button variant="primary">Trigger Step Functions</Button>
+          <hr/>
+          <Button variant="info">Poll Status</Button>
+        </div>
+
       </header>
     </div>
   );
